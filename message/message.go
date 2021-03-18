afb55835c7406f3dabf1914f44976cbaaa10093f
zhengminghui2 <zhengminghui99@gmail.com>
2021-03-18 23:27:06 +0800

    消息类型

--- message/message.go
+++ message/message.go
@@ -4,6 +4,9 @@ type Message struct {
 	MsgType 	MsgType_ 	`json:"msgtype"`
 	Text 		Text_		`json:"text"`
 	Link 		Link_		`json:"link"`
+	Markdown	Markdown_ 	`json:"markdown"`
+	ActionCard  ActionCard_ `json:"actionCard"`
+	FeedCard	FeedCard_	`json:"feedCard"`
 }
 type MsgType_ string
 
@@ -19,20 +22,54 @@ const(
 	text类型
 */
 type Text_ struct {
-	Content string `json:"content"`
-	At At_ `json:"at"`
+	Content 	string 	`json:"content"`
+	At 			At_ 	`json:"at"`
 }
 type At_ struct {
-	AtMobiles []string `json:"atMobiles"`
-	IsAtAll bool `json:"isAtAll"`
+	AtMobiles 	[]string 	`json:"atMobiles"`
+	IsAtAll 	bool 		`json:"isAtAll"`
 }
 
 /*
 	link类型
 */
 type Link_ struct {
-	Text string `json:"text"`
-	Title string `json:"title"`
-	PicUrl string `json:"picUrl"`
-	MessageUrl string `json:"messageUrl"`
+	Text 		string 		`json:"text"`
+	Title 		string 		`json:"title"`
+	PicUrl 		string 		`json:"picUrl"`
+	MessageUrl 	string 		`json:"messageUrl"`
+}
+
+
+/*
+	markdown类型
+*/
+type Markdown_ struct {
+	Title 	string 		`json:"title"`
+	Text 	string 		`json:"text"`
+	At 		At_ 		`json:"at"`
+}
+
+/*
+	整体跳转actionCard
+*/
+type ActionCard_ struct {
+	Title 			string 		`json:"title"`
+	Text 			string 		`json:"text"`
+	BtnOrientation 	string 		`json:"btnOrientation"`
+	SingleTitle	 	string 		`json:"singleTitle"`
+	SingleURL 		string 		`json:"singleUrl"`
+	HideAvatar 		string 		`json:"hideAvatar"`
+	BtnS 			[]Btn_ 		`json:"btns"`
+}
+type Btn_ struct {
+	Title 		string 		`json:"title"`
+	ActionURL 	string 		`json:"actionURL"`
+}
+
+/*
+	FeedCard类型
+*/
+type FeedCard_ struct {
+	Links 		[]Link_ 	`json:"links"`
 }
