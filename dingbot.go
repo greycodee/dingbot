025a5e61a350cf45e6d4b063db010798053ee26f
zhengminghui2 <zhengminghui99@gmail.com>
2021-03-19 10:49:11 +0800

    钉钉机器人

--- dingbot.go
+++ dingbot.go
@@ -1,26 +1,56 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/greycodee/dingbot/message"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 )
-func main() {
-	var text = message.Text_{
-		Content: "22",
-		At: message.At_{
-			AtMobiles: []string{"1111"},
-			IsAtAll:   false,
-		} ,
-	}
 
-	var msg = message.Message{
-		MsgType: message.TextStr,
-		Text: text,
-	}
+
+type DingBot struct{
+	Secret 		string
+	AccessToken string
+}
+
+type Response struct {
+	Errcode int64 `json:"errcode"`
+	Errmsg string `json:"errmsg"`
+}
+
+func (bot *DingBot) send(msg message.Message) (Response,error) {
+	timestamp:=time.Now().UnixNano()/1e6
+	var response Response
+	signStr := sign(timestamp,bot.Secret)
+	dingUrl := fmt.Sprintf("https://oapi.dingtalk.com/robot/send?access_token=%s&timestamp=%d&sign=%s",bot.AccessToken,timestamp,signStr)
 	j,e:=json.Marshal(msg)
 	if e!=nil {
-		fmt.Println(e)
+		return response,e
 	}
-	fmt.Println(string(j))
+	resp,e:=http.Post(dingUrl,"application/json",strings.NewReader(string(j)))
+	if e!=nil {
+		return response,e
+	}
+	defer resp.Body.Close()
+	
+	body, _ := ioutil.ReadAll(resp.Body)
+	e=json.Unmarshal(body,&response)
+	if e!=nil {
+		return response,e
+	}
+	return response,nil
+}
+
+func sign(t int64, secret string) string  {
+	secStr := fmt.Sprintf("%d\n%s",t,secret)
+	hmac256 := hmac.New(sha256.New,[]byte(secret))
+	hmac256.Write([]byte(secStr))
+	result := hmac256.Sum(nil)
+	return base64.StdEncoding.EncodeToString(result)
 }
